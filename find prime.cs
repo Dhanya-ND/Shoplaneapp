652bccb578126dd40de9991920639e04a6c60c58
Dhanya Shree <dhanyashree.d@abylle.com>
2023-08-21 12:13:30 +0530

    Changes done by Dhanya`

---
+++ find prime.cs
@@ -0,0 +1,16 @@
+public string PrimeOrNot(int num)   
+{   
+   int i = 0;   
+   for (i = 3; i < num; i++)   
+   {   
+      if (num % i == 0)   
+      {   
+         Console.WriteLine("{0} is not a prime number", num);   
+         break;   
+      }   
+   }   
+   if (i == num)   
+   {   
+      Console.Writeline("{0} is a prime Number", num);   
+   }   
+   }
